6fc2c0806358f8e18b4ceec379d0a337e86a2fd3
Olsen <krajaolsen@gmail.com>
2020-10-26 18:09:09 +0100

    Changes in homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,24 +7,44 @@
                 </button>
             </router-link>
         </div>
-        <button class="m-2 rounded p-2 bg-green-200"
+        <button class="m-2 rounded p-2 bg-blue-200 border hover:border-blue-700"
                 @click="testAxios">
             Test axios
         </button>
+        <span v-if="isSuccess === true"
+              class="font-bold text-green-600 text-xs">
+            Success
+        </span>
+        <span v-else-if="!isSuccess === false"
+              class="font-bold text-red-600 text-xs">
+            Error
+        </span>
+        <code v-if="responseData"
+             class="flex m-2 p-2 text-sm font-mono bg-gray-300 rounded">
+            {{ responseData }}
+        </code>
     </div>
 </template>
 
 <script>
 export default {
     name: "Home",
+    data() {
+        return {
+            isSuccess: null,
+            responseData: null
+        }
+    },
     methods: {
         testAxios() {
             axios.get('/api/lorem')
                 .then((response) => {
-                    console.log(response.data)
+                    this.responseData = response.data
+                    this.isSuccess = true
                 })
                 .catch((error) => {
-                    console.error(error.data)
+                    this.responseData = error.data
+                    this.isSuccess = false
                 })
         }
     }
