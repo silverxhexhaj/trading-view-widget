c1bca0e65bb4b600237a70ce81e2e33d2873a796
Olsen <krajaolsen@gmail.com>
2020-10-26 17:49:46 +0100

    Added CORS middleware to all the API-routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,21 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::get('lorem', function () {
-    return [
-        'ipsum' => 'dolor'
-    ];
-})->middleware('cors');
+Route::middleware(['api', 'cors'])->group(function () {
+    Route::get('lorem', function () {
+        return [
+            'ipsum' => 'dolor',
+        ];
+    });
+});
