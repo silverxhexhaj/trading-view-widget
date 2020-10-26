4bbb4bd1d50dcbe2e5f8fd6c6b3784a9f42fffa8
Olsen <krajaolsen@gmail.com>
2020-10-26 17:06:02 +0100

    Upgraded to Laravel 8

--- api/routes/web.php
+++ api/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/lorem', function () {
-    return ['ipsum' => 'dolor'];
+Route::get('/', function () {
+    return view('welcome');
 });
